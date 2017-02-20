e6606ddeb4d72d798bb16e57d9076c7069a36950
SirMorde <branpm@gmail.com>
2017-02-20 00:20:11 -0800

    Basic website layout
    Initial layout of website

---
+++ www/js/slider.js
@@ -0,0 +1,13 @@
+// With JQuery
+$("#ex18a").slider({
+  min: 0,
+  max: 10,
+  value: 5,
+  labelledby: 'ex18-label-1'
+});
+$("#ex18b").slider({
+  min: 0,
+  max: 10,
+  value: [3, 6],
+  labelledby: ['ex18-label-2a', 'ex18-label-2b']
+});
